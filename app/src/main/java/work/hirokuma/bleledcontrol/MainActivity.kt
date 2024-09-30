2a3870b666c811c62048a8019c03100fb2bd092e
hirokuma <hiro9ma@gmail.com>
2024-09-30 19:56:12 +0900

    revise

--- app/src/main/java/work/hirokuma/bleledcontrol/MainActivity.kt
+++ app/src/main/java/work/hirokuma/bleledcontrol/MainActivity.kt
@@ -1,7 +1,6 @@
 package work.hirokuma.bleledcontrol
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
@@ -14,7 +13,6 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -51,45 +49,57 @@ class MainActivity : ComponentActivity() {
     // Stops scanning after 10 seconds.
     private val scanPeriod: Long = 10000
 
+    private fun requestPermission(): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            Manifest.permission.BLUETOOTH_SCAN
+        } else {
+            Manifest.permission.ACCESS_FINE_LOCATION
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        val reqPerm = requestPermission()
+        when {
+            ContextCompat.checkSelfPermission(
+                this, reqPerm
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+                Log.d(TAG, "許可あり")
+                return true
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this, reqPerm
+            ) -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected, and what
+                // features are disabled if it's declined. In this UI, include a
+                // "cancel" or "no thanks" button that lets the user continue
+                // using your app without granting the permission.
+                Log.d(TAG, "UIで説明しないといけないらしい")
+                return false
+            }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                Log.d(TAG, "たずねよう")
+                return false
+            }
+        }
+    }
+
     private fun scanLeDevice() {
+        val chk = checkPermission()
+        if (!chk) {
+            val reqPerm = requestPermission()
+            requestPermissionLauncher.launch(reqPerm)
+            return
+        }
         if (!scanning) { // Stops scanning after a pre-defined scan period.
             Log.d(TAG, "scanLeDevice: !scanning")
             handler.postDelayed({
-                scanning = false
-                val reqPerm = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                    Manifest.permission.BLUETOOTH_SCAN
-                } else {
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                }
-                when {
-                    ContextCompat.checkSelfPermission(
-                        this, reqPerm
-                    ) == PackageManager.PERMISSION_GRANTED -> {
-                        // You can use the API that requires the permission.
-                        Log.d(TAG, "許可あり")
-                        scanning = true
-                        Log.d(TAG, "scanLeDevice: startScan")
-                        bluetoothLeScanner.startScan(leScanCallback)
-                    }
-                    ActivityCompat.shouldShowRequestPermissionRationale(
-                        this, Manifest.permission.BLUETOOTH_SCAN
-                    ) -> {
-                        // In an educational UI, explain to the user why your app requires this
-                        // permission for a specific feature to behave as expected, and what
-                        // features are disabled if it's declined. In this UI, include a
-                        // "cancel" or "no thanks" button that lets the user continue
-                        // using your app without granting the permission.
-                        Log.d(TAG, "UIで説明しないといけないらしい")
-                    }
-                    else -> {
-                        // You can directly ask for the permission.
-                        // The registered ActivityResultCallback gets the result of this request.
-                        Log.d(TAG, "たずねよう")
-                        requestPermissionLauncher.launch(reqPerm)
-                    }
-                }
-//                Log.d(TAG, "scanLeDevice: stopScan")
-//                bluetoothLeScanner.stopScan(leScanCallback)
+                scanning = true
+                Log.d(TAG, "scanLeDevice: startScan")
+                bluetoothLeScanner.startScan(leScanCallback)
             }, scanPeriod)
         } else {
             scanning = false
@@ -98,7 +108,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @SuppressLint("MissingPermission")
     private val requestPermissionLauncher =
         registerForActivityResult(
             ActivityResultContracts.RequestPermission()
@@ -107,7 +116,7 @@ class MainActivity : ComponentActivity() {
                 // Permission is granted. Continue the action or workflow in your
                 // app.
                 Log.d(TAG, "requestPermissionLauncher: isGranted")
-                bluetoothLeScanner.startScan(leScanCallback)
+                scanLeDevice()
             } else {
                 // Explain to the user that the feature is unavailable because the
                 // feature requires a permission that the user has denied. At the
@@ -130,9 +139,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    companion object {
-        private val TAG = javaClass.simpleName
-    }
+    private val TAG = javaClass.simpleName
 }
 
 @Composable
