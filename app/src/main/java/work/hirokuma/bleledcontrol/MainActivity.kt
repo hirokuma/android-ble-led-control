3b1b8884486ce2091d20947a9873c03b1f5e0732
hirokuma <hiro9ma@gmail.com>
2024-10-16 19:31:44 +0900

    LazyColumn

--- app/src/main/java/work/hirokuma/bleledcontrol/MainActivity.kt
+++ app/src/main/java/work/hirokuma/bleledcontrol/MainActivity.kt
@@ -1,10 +1,12 @@
 package work.hirokuma.bleledcontrol
 
 import android.Manifest
-import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
@@ -13,36 +15,53 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import work.hirokuma.bleledcontrol.ui.theme.BLELEDControlTheme
+import work.hirokuma.bleledcontrol.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var bluetoothLeScanner: BluetoothLeScanner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val bluetoothManager = getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter = bluetoothManager.adapter
+        bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+
+        scanLeDevice()
         setContent {
-            BLELEDControlTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    scanLeDevice()
-                    Greeting("Android")
-                }
+            AppTheme {
+                DeviceScreen(listOf())
             }
         }
     }
 
-    private val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-    private val bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
     private var scanning = false
     private val handler = Handler(Looper.getMainLooper())
 
@@ -143,17 +162,74 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DeviceList(list: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier.fillMaxSize()
+    ) {
+        items(list) { item ->
+            Row(
+                modifier = Modifier
+                    .height(48.dp)
+                    .border(width = 1.dp, color = colorScheme.primaryContainer)
+                    .clickable { Log.d("row", "click: $item") }
+            ) {
+                Spacer(Modifier.width(16.dp))
+                Text(
+                    text = ":$item",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(alignment = Alignment.CenterVertically),
+                    fontSize = 24.sp,
+                )
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    BLELEDControlTheme {
-        Greeting("Android")
+fun DeviceScreen(list: List<String>, modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = colorScheme.primaryContainer,
+                    titleContentColor = colorScheme.primary,
+                ),
+                title = {
+                    Text(stringResource(R.string.app_name))
+                }
+            )
+        },
+        // bottomBar = {}
+    ) { innerPadding ->
+        Surface(
+            modifier = modifier.fillMaxSize(),
+            color = colorScheme.background,
+            contentColor = colorScheme.onBackground,
+        ) {
+            DeviceList(
+                list = list,
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "light mode"
+)
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "dark mode"
+)
+@Composable
+fun DeviceListPreview() {
+    AppTheme {
+        val dummyData: List<String> = listOf("a1", "b2", "c3")
+        DeviceScreen(dummyData)
     }
 }
