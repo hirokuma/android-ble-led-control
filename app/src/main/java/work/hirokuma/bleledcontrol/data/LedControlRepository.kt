ba6e2c206d042026ea5db74289dd3e9fffd105e7
hirokuma <hiro9ma@gmail.com>
2024-10-29 14:06:27 +0900

    wip: return Flow on BLE scan

--- app/src/main/java/work/hirokuma/bleledcontrol/data/LedControlRepository.kt
+++ app/src/main/java/work/hirokuma/bleledcontrol/data/LedControlRepository.kt
@@ -1,6 +1,7 @@
 package work.hirokuma.bleledcontrol.data
 
 import android.util.Log
+import kotlinx.coroutines.flow.Flow
 import work.hirokuma.bleledcontrol.data.ble.BleScan
 
 private const val TAG = "LedControlRepository"
@@ -8,6 +9,7 @@ private const val TAG = "LedControlRepository"
 interface LedControlRepository {
     val searching: Boolean
     fun startDeviceSearch(callback: (Device) -> Unit)
+    fun getDevices(): Flow<Device>
     fun stopDeviceSearch()
 }
 
@@ -23,6 +25,8 @@ class BleLedControlRepository(
             callback(device)
         }
     }
+
+    override fun getDevices(): Flow<Device> = bleScan.getScanResults()
 
     override fun stopDeviceSearch() {
         bleScan.stopScan()
--- app/src/main/java/work/hirokuma/bleledcontrol/data/ble/BleScan.kt
+++ app/src/main/java/work/hirokuma/bleledcontrol/data/ble/BleScan.kt
@@ -1,11 +1,15 @@
 package work.hirokuma.bleledcontrol.data.ble
 
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.util.Log
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import work.hirokuma.bleledcontrol.data.Device
 
 private const val TAG = "BleScan"
@@ -39,6 +43,45 @@ class BleScan(context: Context) {
             return false
         }
         return true
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getScanResults(): Flow<Device> = callbackFlow {
+        if (scanning) {
+            Log.e(TAG, "already scanning")
+            return@callbackFlow
+        }
+        val callback = object : ScanCallback() {
+            override fun onScanResult(callbackType: Int, result: ScanResult) {
+                super.onScanResult(callbackType, result)
+                val record = result.scanRecord ?: return
+                if (record.deviceName == null) {
+                    return
+                }
+                trySend(Device(
+                            address = result.device.address,
+                            name = record.deviceName!!,
+                            ssid = result.rssi,
+                            scanRecord = record
+                        )
+                )
+            }
+        }
+
+        try {
+            Log.d(TAG, "scanLeDevice: startScan")
+            bluetoothLeScanner.startScan(callback)
+            scanning = true
+        }
+        catch (e: SecurityException) {
+            Log.e(TAG, "startScan: $e")
+            return@callbackFlow
+        }
+
+        awaitClose {
+            bluetoothLeScanner.stopScan(callback)
+            scanning = false
+        }
     }
 
     fun stopScan(): Boolean {
